f58eda567aed2f3e42d7cf9b9e935e94f1b898be
Cristina Castro <cristinacastro87@gmail.com>
2020-10-15 14:20:20 +0200

    enlace a otra página funcionando (por fin)

--- scripts/api.js
+++ scripts/api.js
@@ -1,67 +1,85 @@
 
 
+const urlParams = new URLSearchParams(window.location.search); 
+const buscadorInput = urlParams.get('search');
+
 //RESULTATS PER BUSCADOR
+
 //funció per agafar el valor introduit al buscador
- const buscador = document.querySelector('#buscador-form') //aqui posem el id del form id
+const buscador = document.querySelector('#buscador-form') //aqui posem el id del form id
 console.log(buscador);
+const btnId = document.getElementById('ingredient-search');
 
-    buscador.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const buscador = document.querySelector('#ingredient-search').value; //aqui posem el id del input id
-    getInputsByValue(buscador);
-    console.log("dgf");
-}); 
+function loadDate (){
+    let url="recipes.html?search="+encodeURIComponent(btnId.value)
+    console.log(url, "holaa")
+    window.location.href=url
+  }
 
+    if(buscador){
+
+        buscador.addEventListener('submit', (event) => {
+            event.preventDefault();
+            loadDate();
+            //getInputsByValue(buscadorInput);
+        }); 
+        
+
+}
 
 //funció per desplegar receptes en funció del valor introduit previament per l'usuari
 function getInputsByValue(buscador) {
 
-    /* redirect = () => {
-        setTimeout ( () => location.assign('recipes.html'), 2000);
-    } */
-    //window.location.href = 'recipes.html';
-    console.log("hola")
-    fetch(`https://api.spoonacular.com/recipes/complexSearch?intolerances=gluten&type=breakfast&number=10&apiKey=88dc5764dc764ea3b6b5c87bf1a92ef0&query=${buscador.toLowerCase()}`)
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?intolerances=gluten&type=breakfast&number=10&apiKey=88dc5764dc764ea3b6b5c87bf1a92ef0&query=${buscador}`)
         .then((response) => {
             return response.json()
         })
         .then((data) => {
             console.log(data);
+            const section = document.getElementById("recipes-list"); //"recipes-list" page recipes o "mostra-content-api" a linici
             section.innerHTML = "";
-            data.items.forEach((recipe) => {
-                const section = document.getElementById("recipes-list");
+            data.results.forEach((recipe) => {
                 const article = document.createElement('article');
                 article.innerHTML = `
                 <h2>${recipe.title}</h2>
-                ${recipe.image}
-                <a href=#recipe-product.html"> <button class="recipe-category-btn >See recipe</button></a>
+                <img class= recipe-general-info-list src= "${recipe.image}">
+                <button class="recipe-general-info button">See recipe</button>
                 `;
-                  section.appendChild(article);
+                article.className = "recipe-general-info";
+                section.appendChild(article);
+
             })
         })
        
-     .catch((err) => {})
+     .catch((err) => {console.log(err)})
 }
 
+if (buscadorInput){
+    getInputsByValue(buscadorInput);
 
+}
 
 //RESULTATS PER CATEGORIES
 
-function getRecipes(categorý) {
-    const recipesSuggestions = document.querySelector('.recipes-suggestions')//aqui agafem el lloc on v
-    const section = document.querySelector(".recipes-list");
+
+function getRecipes(category) {
+    const section = document.getElementById('mostra-content-api')//aqui agafem el lloc on volem desplegar la info
 
     fetch("https://api.spoonacular.com/recipes/complexSearch?intolerances=gluten&type=breakfast&number=200&apiKey=88dc5764dc764ea3b6b5c87bf1a92ef0")
-        .then((response) => {
 
-            return response.json()
-        })
+    .then((response) => {
+
+        const recipesJson = response.json()    
+        
 
         const bowlsArr = recipesJson.results.filter(x=>x.id===647261 || x.id===1095752 || x.id===639131 || x.id===1095771  || x.id===663853 || x.id===637705)
         const smoothiesArr = recipesJson.results.filter(x=>x.id===638825 || x.id===1096176 || x.id===655786 || x.id===1096178  || x.id===641411 || x.id===650485 || x.id===645530 || x.id===660227)
         const cookedArr = recipesJson.results.filter(x=>x.id===643514 || x.id===1096257 || x.id===1096166 || x.id===1096262  || x.id===665734 || x.id===664280 || x.id===1096024)
             
-        
+        return recipesJson
+
+        })
+    
             .then((data) => {
                 console.log(data);
                  section.innerHTML = "";
@@ -118,14 +136,21 @@ function getRecipes(categorý) {
 
 
 //botons per cada categoria. Li passarem la funció de getRecipes perquè ens doni els resultats de cada categoria (seran a la mateixa pàgina la anomenada recipes.html)
-const bowlsButton = document.querySelector('#bowls')
-bowlsButton.addEventListener('click', () => getRecipes("bowls"));
 
-const smoothiesButton = document.querySelector('#smoothies')
-smoothiesButton.addEventListener('click', () => getRecipes("smoothies"));
 
-const cookedButton = document.querySelector('#cooked')
-cookedButton.addEventListener('click', () => getRecipes("cooked"));
+/* const bowlsButton = document.getElementById('input-button-bowls')
+bowlsButton.onclick = function (){
+    return
+}
+bowlsButton.addEventListener('click', () => return bowlsDiv);
+console.log("holaaa")
+
+const smoothiesButton = document.getElementById('input-button-smoothies')
+smoothiesButton.addEventListener('click', () => );
+
+const cookedButton = document.getElementById('input-button-cooked')
+cookedButton.addEventListener('click', () => r); 
+ */
 
 
 //RESULTAT RECEPTA DETALLADA
@@ -133,7 +158,7 @@ function expandRecipe (id){
     const extendedInfo = document.querySelector('#extended-info')
     const section = document.querySelector(".recipe-extended-info");
 
-    window.location.href = 'recipe-product.html';
+    //window.location.href = 'recipe-product.html';
     console.log("jjkn")
     fetch(`https://api.spoonacular.com/recipes/${id}/information?apiKey=88dc5764dc764ea3b6b5c87bf1a92ef0`)
         .then((response) => {
